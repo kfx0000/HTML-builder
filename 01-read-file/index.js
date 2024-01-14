2660a06060ca31c24fe1a36d57535406e35d6f2c
Andy <kfx0000@gmail.com>
2024-01-14 18:02:06 +0300

    feat: add 01-read-file solution

---
+++ 01-read-file/index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+const file = 'text.txt';
+const filePath = path.join(__dirname, file);
+const streamToRead = fs.createReadStream(filePath);
+streamToRead.on('data', (x) => console.log(x.toString()));
