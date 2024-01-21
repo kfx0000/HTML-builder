c0cfbfea0560f9054a5dce8ac8b69a42ac9b245c
Andy <kfx0000@gmail.com>
2024-01-21 20:25:45 +0300

    fix: mistake in 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,6 +6,12 @@ const dirSrc = path.join(__dirname, src);
 const dirDst = path.join(__dirname, dst);
 
 fs.mkdir(dirDst, { recursive: true }, () => {});
+fs.readdir(dirDst, (err, files) => {
+    if (err) console.log(err.message); else
+    files.forEach((file) => {
+        fs.unlink(path.join(dirDst, file), () => {});
+    });
+});
 fs.readdir(dirSrc, (err, files) => {
     if (err) console.log(err.message); else
     files.forEach((file) => fs.copyFile(path.join(dirSrc, file), path.join(dirDst, file), () => {}));
