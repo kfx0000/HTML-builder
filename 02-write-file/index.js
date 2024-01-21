f88e99d913f5369d7df1dd4c701acc3eebcd5adf
Andy <kfx0000@gmail.com>
2024-01-21 12:41:12 +0300

    feat: add 02-write-file solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const file = 'text.txt';
+const filePath = path.join(__dirname, file);
+const { stdin, stdout } = process;
+const streamToWrite = fs.createWriteStream(filePath);
+
+stdout.write('Hello! Input a text:\n');
+stdin.on('data', (data) => {
+    if (data.toString().toLowerCase().includes('exit')) process.exit();
+    streamToWrite.write(data, 'utf-8');
+    }
+);
+
+process.on('SIGINT', () => process.exit());
+process.on('exit', () => console.log('Have a nice day!'));
